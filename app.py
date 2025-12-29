49d82954056aed77e8c0bc568a79e8a63790b5c6
tanmay15singh-lang <tanmay15singh@gmail.com>
2025-12-29 12:19:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from rules import evaluate_content
+import os
 
 app = Flask(__name__)
 
+FRONTEND_DIR = os.path.join(os.getcwd(), "Frontend")
+
+# Serve frontend
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return send_from_directory(FRONTEND_DIR, "index.html")
 
+# Text analysis API
 @app.route("/analyze", methods=["POST"])
 def analyze():
     data = request.get_json()
